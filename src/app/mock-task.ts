0de662b1d356ae370a1718302569ecb0c09e8a7a
Hampus Lagsten <hampus@citium.tech>
2023-01-09 08:29:11 +0100

    initial commit

---
+++ src/app/mock-task.ts
@@ -0,0 +1,30 @@
+import {Task} from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors appointment',
+    day: 'may 5th at 2:30pm',
+    reminder: true,
+},{
+  id: 2,
+  text: 'Angular Crash Course',
+  day: 'jan 19th at 4:00pm',
+  reminder: false,
+},{
+  id: 3,
+  text: 'Lucas birthday',
+  day: 'may 23rd at 7:00am',
+  reminder: true,
+},{
+  id: 4,
+  text: 'Thailand trip',
+  day: 'jan 29th at 12:05am',
+  reminder: true,
+},{
+  id: 5,
+  text: 'Food Shopping',
+  day: 'aug 1se at 2:30pm',
+  reminder: false,
+}
+]
